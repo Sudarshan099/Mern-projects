148aaf86ea67c5a48e1a26ced7f7577ced2e32af
sudarshan099 <sudarshanbam2021@gmail.com>
2024-07-19 09:01:21 +0545

    backend

---
+++ HealBackend/fsmodule.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+
+fs.readFile("node.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
+
+// Create or Update (writeFile)
+fs.writeFile("node.txt", "This is the initial content", "utf8", (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log("File has been created/updated");
+
+  // Read the file
+  fs.readFile("node.txt", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("Read file content:", data);
+
+    // Update (appendFile)
+    fs.appendFile("node.txt", "\nThis content is appended", "utf8", (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("New content has been appended");
+
+      // Read the updated file
+      fs.readFile("node.txt", "utf8", (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log("Updated file content:", data);
+
+        // Delete the file
+        // fs.unlink("node.txt", (err) => {
+        //   if (err) {
+        //     console.error(err);
+        //     return;
+        //   }
+        //   console.log("File has been deleted");
+        // });
+      });
+    });
+  });
+});
---
+++ HealBackend/index.js
@@ -0,0 +1,51 @@
+// const math = require("./math");
+// const fsmodule = require("./fsmodule");
+
+// let sum = math.add(8, 8);
+// console.log(sum);
+
+// let diff = math.sub(60, 57);
+// console.log(diff);
+
+// fsmodule;
+
+// const http = require("http");
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "text/html" });
+//   res.write("hello griffin");
+//   res.end();
+// });
+// server.listen(3000, () => {
+//   console.log("server is running");
+// });
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello sudarshan");
+});
+
+const users = [
+  { id: 1, name: "sudrashan" },
+  { id: 2, name: "Griffin" },
+];
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.get("/getUsers/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const user = users.find((user) => user.id === id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).send("User not Found");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
